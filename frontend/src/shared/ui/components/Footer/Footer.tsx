1e3228219f7ac132163d8bf6e3f6be92b5d5dfe5
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-11-23 12:50:23 +0200

    upd

--- frontend/src/shared/ui/components/Footer/Footer.tsx
+++ frontend/src/shared/ui/components/Footer/Footer.tsx
@@ -4,34 +4,33 @@ import { Logo } from "../Logo";
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-			<div className={styles.content}>
-				<Logo />
+      <div className={styles.content}>
+        <Logo />
 
-				<div className={styles.context}>
-					<h3>Проект</h3>
-					<p className={styles.text}>Наш Github</p>
-					<p className={styles.text}>Презентация</p>
-				</div>
+        <div className={styles.context}>
+          <h3>Проект</h3>
+          <a className={styles.text}>Наш Github</a>
+          <a className={styles.text}>Презентация</a>
+        </div>
 
-				<div className={styles.context}>
-					<h3>Сайт</h3>
-					<p className={styles.text}>Новости</p>
-					<p className={styles.text}>Мероприятия</p>
-				</div>
+        <div className={styles.context}>
+          <h3>Сайт</h3>
+          <a className={styles.text}>Новости</a>
+          <a className={styles.text}>Мероприятия</a>
+        </div>
 
-				<div className={styles.context}>
-					<h3>О нас</h3>
-					<p className={styles.text}>Связь</p>
-					<p className={styles.text}>Команда</p>
-				</div>
+        <div className={styles.context}>
+          <h3>О нас</h3>
+          <a className={styles.text}>Связь</a>
+          <a className={styles.text}>Команда</a>
+        </div>
 
-				<div className={styles.context}>
-					<h3>Спонсоры</h3>
-					<p className={styles.text}>ВФСК ГТО</p>
-					<p className={styles.text}>Миниспорт Россия</p>
-				</div>
-
-			</div>
+        <div className={styles.context}>
+          <h3>Спонсоры</h3>
+          <a className={styles.text}>ВФСК ГТО</a>
+          <a className={styles.text}>Миниспорт Россия</a>
+        </div>
+      </div>
     </footer>
   );
 };
